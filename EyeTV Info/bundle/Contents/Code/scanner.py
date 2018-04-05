906b49b38b476023f29af742d5551c8a58030982
Stephen Collie <ste@mini2016.local>
2018-04-05 20:29:22 +0100

    Updates to scanner to read UK EPG data

--- EyeTV Info/bundle/Contents/Code/scanner.py
+++ EyeTV Info/bundle/Contents/Code/scanner.py
@@ -1,48 +1,94 @@
-import Media, plistlib
+import Media, plistlib, re
 
 recdSuffix  = ".eyetv"
 metaSuffix  = ".eyetvp"
 liveSuffix  = "Live TV Buffer.eyetv"
 videoSuffix = ".mpg"  # Use .m4v if you needed
 
-def Scan(path, files, mediaList, subdirs, isOfGenre):
+def ScanTVShow(path, files, mediaList, subdirs):
     """Scans the path for an EyeTV recording of the given genre.
+   
+    An EyeTV recording consists of the actual video file(s) and a set
+    of metadata files.  First makes sure that the given files do exist.
+    Then inspect the metadata to see if the recording belongs to the
+    desired genre.
+
+    """
+    if len(files) < 1:
+        return
+    if path.endswith(recdSuffix) and not path.endswith(liveSuffix):
+      recording = None
+      metadata  = None
+
+      for file in files:
+        if file.endswith(videoSuffix):
+            recording = file
+        if file.endswith(metaSuffix):
+            metadata  = file
+        if recording and metadata:
+            break
+
+      if recording and metadata:
+        metadataPList = plistlib.readPlist(metadata)
+
+        title = lookupPlist(metadataPList, 'epg info', 'TITLE')
+        abstract = lookupPlist(metadataPList, 'epg info', 'ABSTRACT')
+        findEpisode = re.search('(\d+)\/.*', abstract)
+        episode = ""
+        if findEpisode:
+          episode = findEpisode.group(1)
+
+        print abstract
+        print "Found " + title + ", episode " + episode
+        episode = Media.Episode(title, "1", episode, abstract)
+        episode.parts.append(recording)
+        mediaList.append(episode)
+
+def ScanMovie(path, files, mediaList, subdirs):
+    """Scans the path for an EyeTV recording of the given genre.
+
     
     An EyeTV recording consists of the actual video file(s) and a set
     of metadata files.  First makes sure that the given files do exist.
     Then inspect the metadata to see if the recording belongs to the
     desired genre.
 
-    isOfGenre is a function that takes a plist as its argument.
-    
     """
-
     if len(files) < 1:
         return
     if path.endswith(recdSuffix) and not path.endswith(liveSuffix):
-        recording = FindRecording(files, isOfGenre)
-        if recording:
-            movie = Media.Movie(path.split('/')[-1]) # Last part of the path
-            movie.parts.append(recording)
-            mediaList.append(movie)
+      recording = None
+      metadata  = None
 
-def FindRecording(files, isOfGenre):
-    """Attempts to locate a recording of the given genre among files.
-
-    Scan for a video file and metadata file, and then apply isOfGenre
-    on the plist read from the metadata file.
-    """
-    recording = None
-    metadata  = None
-
-    for file in files:
+      for file in files:
         if file.endswith(videoSuffix):
             recording = file
         if file.endswith(metaSuffix):
             metadata  = file
         if recording and metadata:
-            if isOfGenre(plistlib.readPlist(metadata)):
-                return recording
+            break
 
-    return None
+      if recording and metadata:
+        metadataPList = plistlib.readPlist(metadata)
+
+        title = lookupPlist(metadataPList, 'epg info', 'TITLE')
+        abstract = lookupPlist(metadataPList, 'epg info', 'ABSTRACT')
+        findYear = re.search('.*((19|20)\d{2}).*', abstract)
+        year = ""
+        if findYear:
+          year = findYear.group(1)
+ 
+        print "Found " + title + ", year " + year
+        movie = Media.Movie(title, year)
+        movie.parts.append(recording)
+        mediaList.append(movie)
+
+def lookupPlist(plist, *args):
+    for key in args:
+        try:
+            plist = plist[key]
+        except KeyError:
+            return None
+    return plist
+
                         
